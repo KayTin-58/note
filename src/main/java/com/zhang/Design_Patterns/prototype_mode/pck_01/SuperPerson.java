b39769c549a47a9c37dd575f8307dd1c9a5acfed
张波 <bo.zhang04@hand-china.com>
2019-05-22 00:15:10 +0800

    新增原型模式

---
+++ src/main/java/com/zhang/Design_Patterns/prototype_mode/pck_01/SuperPerson.java
@@ -0,0 +1,33 @@
+package com.zhang.Design_Patterns.prototype_mode.pck_01;
+
+/**
+ * description
+ *
+ * @author zb 2019/05/19 17:31
+ */
+public class SuperPerson implements Cloneable{
+
+   private String skill;
+   private Person person;
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
---
+++ src/main/java/com/zhang/Design_Patterns/prototype_mode/pck_02/JinGuBang.java
@@ -0,0 +1,16 @@
+package com.zhang.Design_Patterns.prototype_mode.pck_02;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * description
+ *
+ * @author zb 2019/05/19 19:31
+ */
+@Setter
+@Getter
+public class JinGuBang {
+    private Integer w;
+    private Integer h;
+}

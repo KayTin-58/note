d77b43f9f8e1ee723c09d656e06b90e87f53125f
张波 <bo.zhang04@hand-china.com>
2019-06-17 12:00:11 +0800

    设计模式

---
+++ src/main/java/com/zhang/Design_Patterns/decorator_mode/pck_01/IPancake.java
@@ -0,0 +1,13 @@
+package com.zhang.Design_Patterns.decorator_mode.pck_01;
+
+/**
+ * 定义一个煎饼接口
+ * Created by BrightLoong on 2018/4/22.
+ */
+public interface IPancake {
+    /**
+     * 定义烹饪的操作
+     */
+    void cook();
+}
+

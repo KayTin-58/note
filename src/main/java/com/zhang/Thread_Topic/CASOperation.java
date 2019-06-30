ab76539ff480871d654f73a195ed83fdfe62c3a5
张波 <bo.zhang04@hand-china.com>
2019-07-01 00:03:07 +0800

    CAS使用案例

--- src/main/java/com/zhang/Thread_Topic/CASOperation.java
+++ src/main/java/com/zhang/Thread_Topic/CASOperation.java
@@ -1,4 +1,4 @@
-package com.zhang.thread;
+package com.zhang.Thread_Topic;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.CountDownLatch;
@@ -8,28 +8,28 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 这里将会介绍如何实现原子性的常见手段以及java中的处理当方法
- *
+ * <p>
  * 1、通过锁住总线来保证：简而言之就是当一个变量被多个cpu操作的时候，在一个cpu获得对这个变量的操作全之后
  * 系统会锁住cpu和内存的通信，使得其他cpu无法访问内存。（不建议使用这种策略）
- *
- *
+ * <p>
+ * <p>
  * 2、使用缓存策略
- *
- *
+ * <p>
+ * <p>
  * java中的策略
- *    1、借助于CAS操作实现（需要考虑通过version来防止ABA问题）
- *    2、借助于锁实现
- *
- *
+ * 1、借助于CAS操作实现（需要考虑通过version来防止ABA问题）
+ * 2、借助于锁实现
+ * <p>
+ * <p>
  * Created by zb on 2018/12/18.
  */
 public class CASOperation {
-     static int i = 0;
-     static  AtomicInteger atomicInteger = new AtomicInteger(0);
+    static int i = 0;
+    static AtomicInteger atomicInteger = new AtomicInteger(0);
+    static CountDownLatch countDownLatch = new CountDownLatch(200);
+    static Counter counter = new Counter(countDownLatch);
 
-    static Counter counter = new Counter();
-
-    public static void main(String[]args)throws Exception{
+    public static void main(String[] args) throws Exception {
         //这里使用线程池启动执行100个线程
         //线程池的参数
         /**@param corePoolSize the number of threads to keep in the pool, even
@@ -43,53 +43,65 @@ public class CASOperation {
          * @param workQueue the queue to use for holding tasks before they are
          */
         ThreadPoolExecutor executor = new ThreadPoolExecutor(10, 10, 100, TimeUnit.MILLISECONDS,
-                new ArrayBlockingQueue<Runnable>(10));
-        CountDownLatch countDownLatch = new CountDownLatch(100);
+                new ArrayBlockingQueue<Runnable>(109));
 
-        for(int i = 0;i<100;i++ ) {
-            executor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    counter.safeCount();
-                    counter.unSafeCount();
-                }
-            });
+        for (int i = 0; i < 100; i++) {
+            executor.execute(() -> {
+                        counter.safeCount();
+                        counter.unSafeCount();
+                    }
+            );
         }
-
+        /**
+         * 主线程会等待
+         */
         countDownLatch.await();
-
-        System.out.println("线程安全的计数："+atomicInteger.get());
-        System.out.println("线程不安全的计数："+atomicInteger.get());
+        System.out.println("线程安全的计数：" + atomicInteger.get());
+        System.out.println("线程不安全的计数：" + i);
     }
 
 
+    static class Counter {
 
-
-
-    static  class Counter {
-
+        CountDownLatch countD = null;
+        public Counter (CountDownLatch countDownLatch) {
+            this.countD = countDownLatch;
+        }
         //线程安全的计数
         public void safeCount() {
-            while(true) {
-                int  i = atomicInteger.get();
+            /**
+             * 这里会自旋更新
+             */
+            while (true) {
+                //System.out.println(System.currentTimeMillis());
+                int i = atomicInteger.get();
                 //注意这个函数的额用法
                 /**
+                 *
                  * @param expect the expected value
                  * @param update the new value
                  * @return {@code true} if successful. False return indicates that
                  * the actual value was not equal to the expected value.
                  */
-                boolean falg = atomicInteger.compareAndSet(i,i++);
+                boolean falg = atomicInteger.compareAndSet(i, i++);
                 //代表更新成功
-                if(falg) {
+                if (falg) {
+                    this.countD.countDown();
+                    System.out.println(atomicInteger.get());
                     break;
                 }
+
             }
+          /*  atomicInteger.getAndAdd(1);
+            this.countD.countDown();*/
         }
 
         //线程不安全的计数
         public void unSafeCount() {
-              i++;  //等价于  i = i+1;
+
+            i++;  //等价于  i = i+1;
+            System.out.println("usSave:" + i);
+            this.countD.countDown();
         }
     }
 }
